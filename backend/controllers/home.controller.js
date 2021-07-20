a5e4c01a027fa11e6e3cfc084f90c1ba0e306cec
xand974 <xand974@gmail.com>
2021-07-20 21:23:13 +0400

    ajout premiere routes

--- backend/controllers/home.controller.js
+++ backend/controllers/home.controller.js
@@ -1,13 +1,27 @@
-const NoteModel = require("../models/Note");
+const Note = require("../models/Note");
 
 module.exports = {
   all_notes_get: async (req, res) => {
     try {
-      const listFound = await NoteModel.find();
+      const listFound = await Note.find();
       if (!listFound) return res.json(["no list found"]);
       return res.status(200).json(listFound);
     } catch (err) {
-      return res.json(err);
+      return res.status(500).json(err);
+    }
+  },
+  create_notes_post: async (req, res) => {
+    try {
+      const { title, content } = req.body;
+      var note = new Note({
+        title,
+        content,
+      });
+
+      await note.save();
+      return res.json(note);
+    } catch (err) {
+      return res.status(500).json(err);
     }
   },
 };
--- backend/routes/home.route.js
+++ backend/routes/home.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const home_controller = require("../controllers/home.controller");
 
-router.get("/home", home_controller);
+router.get("/home", home_controller.all_notes_get);
+router.post("/create", home_controller.create_notes_post);
 
 module.exports = router;

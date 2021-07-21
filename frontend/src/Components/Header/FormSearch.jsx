315293416415b3f6f1c8bb998bbfdad2deffe466
xand974 <xand974@gmail.com>
2021-07-21 13:10:21 +0400

    ajout axios

--- frontend/src/Components/Header/FormSearch.jsx
+++ frontend/src/Components/Header/FormSearch.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FormSearch.css";
 
 export default function FormSearch() {
+  const [userInput, setUserInput] = useState("");
+  var HandleChange = (e) => {
+    const search = e.target.value;
+    setUserInput(search);
+  };
   return (
     <div className="search__container">
       <form>
-        <input spellCheck={false} type="text" />
+        <input
+          spellCheck={false}
+          onChange={HandleChange}
+          value={userInput}
+          type="text"
+          name="title"
+        />
         <button>Rechercher</button>
       </form>
     </div>

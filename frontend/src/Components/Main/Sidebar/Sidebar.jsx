3a48129a95f60ac7c786b93f6d10e7dfe44af43a
xand974 <xand974@gmail.com>
2021-07-24 19:19:45 +0400

    ajout sidebar

--- frontend/src/Components/Main/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Main/Sidebar/Sidebar.jsx
@@ -1,5 +1,59 @@
 import "./Sidebar.css";
-
+import { Chat, Group, VideoCall, Wifi } from "@material-ui/icons";
+import defaultProfilePicture from "../../../Images/user__default.jpeg";
 export default function Sidebar() {
-  return <div className="sidebar__container">sidebar</div>;
+  return (
+    <div className="sidebar__container">
+      <div className="sidebar__links">
+        <li>
+          <Wifi style={{ fontSize: "25px" }} />
+          <span>Feed</span>
+        </li>
+        <li>
+          <Chat style={{ fontSize: "25px" }} />
+          <span>Chat</span>
+        </li>
+        <li>
+          <VideoCall style={{ fontSize: "25px" }} />
+          <span>Video</span>
+        </li>
+        <li>
+          <Group style={{ fontSize: "25px" }} />
+          <span>Groups</span>
+        </li>
+        <li>
+          <Wifi style={{ fontSize: "25px" }} />
+          <span>Feed</span>
+        </li>
+        <button className="sidebar__button">Show More</button>
+        <hr className="sidebar__line" />
+        <ul className="sidebar__profile">
+          <div>
+            <img src={defaultProfilePicture} alt="" />
+            <li className="profile__li">Alexandre Malet</li>
+          </div>
+          <div>
+            <img src={defaultProfilePicture} alt="" />
+            <li className="profile__li">Alexandre Malet</li>
+          </div>
+          <div>
+            <img src={defaultProfilePicture} alt="" />
+            <li className="profile__li">Alexandre Malet</li>
+          </div>
+          <div>
+            <img src={defaultProfilePicture} alt="" />
+            <li className="profile__li">Alexandre Malet</li>
+          </div>
+          <div>
+            <img src={defaultProfilePicture} alt="" />
+            <li className="profile__li">Alexandre Malet</li>
+          </div>
+          <div>
+            <img src={defaultProfilePicture} alt="" />
+            <li className="profile__li">Alexandre Malet</li>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
 }

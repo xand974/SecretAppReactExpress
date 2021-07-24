3bbcdb91ce932138d54e38949ea9421c2a5bc395
xand974 <xand974@gmail.com>
2021-07-24 21:55:58 +0400

    ajout btn vert quand on est connecté

--- frontend/src/Components/Main/Contact/OnlineFriend/OnlineFriend.jsx
+++ frontend/src/Components/Main/Contact/OnlineFriend/OnlineFriend.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import image from "../../../../Images/default-user-image.png";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+
 import "./OnlineFriend.css";
 export default function OnlineFriend() {
   return (
     <div className="online__main">
       <div className="online__image__container">
         <img className="online__image" src={image} alt="profile" />
+        <FiberManualRecordIcon className="online" />
       </div>
       <div>
         <p className="online__name">Morgane Lartin</p>

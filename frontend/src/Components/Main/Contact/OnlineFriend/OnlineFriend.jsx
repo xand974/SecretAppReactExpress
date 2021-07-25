341eaf9d26301cd0dc0678006ccb737d0f8d96c2
xand974 <xand974@gmail.com>
2021-07-25 19:04:29 +0400

    ajout data right bar

--- frontend/src/Components/Main/Contact/OnlineFriend/OnlineFriend.jsx
+++ frontend/src/Components/Main/Contact/OnlineFriend/OnlineFriend.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import image from "../../../../Images/default-user-image.png";
+import defaultPic from "../../../../Images/default-user-image.png";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 import "./OnlineFriend.css";
-export default function OnlineFriend() {
+export default function OnlineFriend({ username, picture }) {
   return (
     <div className="online__main">
       <div className="online__image__container">
-        <img className="online__image" src={image} alt="profile" />
+        <img
+          className="online__image"
+          src={picture || defaultPic}
+          alt="profile"
+        />
         <FiberManualRecordIcon className="online" />
       </div>
       <div>
-        <p className="online__name">Morgane Lartin</p>
+        <p className="online__name">{username}</p>
       </div>
     </div>
   );

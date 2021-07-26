bc471368e825a0de5dce1f722f57664d01462685
xand974 <xand974@gmail.com>
2021-07-26 22:50:46 +0200

    ajout bar infos user sur le profile

--- frontend/src/Components/Profile/InfoUser/InfoUser.jsx
+++ frontend/src/Components/Profile/InfoUser/InfoUser.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import "./InfoUser.css";
+import OnlineFriend from "../../Main/Contact/OnlineFriend/OnlineFriend";
 
 export default function InfoUser() {
-  return <div>Info</div>;
+  return (
+    <div className="profile__infos__user">
+      <div className="profile__infos">
+        <h5>User Informations</h5>
+        <div>
+          <li>
+            <strong>City :</strong> London
+          </li>
+          <li>
+            <strong>From : </strong>Réunion
+          </li>
+          <li>
+            <strong>RelationShip :</strong> Single
+          </li>
+        </div>
+      </div>
+      <div className="profile__friends">
+        <h5>User Friends</h5>
+        <div>
+          <OnlineFriend username="Pomme" />
+        </div>
+      </div>
+    </div>
+  );
 }

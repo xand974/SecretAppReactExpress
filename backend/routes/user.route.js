cb8614b840c57bad99b399f241cfb011347f37b3
xand974 <xand974@gmail.com>
2021-07-27 23:13:48 +0200

    correction cookie

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -11,7 +11,7 @@ router.post("/login", cookie.redirectHome, user_controller.login_post);
 router.post("/logout", cookie.authorize, user_controller.logout_post);
 
 router.post("/follow/:id", cookie.authorize, user_controller.follow_post);
-router.get("/:id", cookie.authorize, user_controller.user_get);
+router.get("/:id", user_controller.user_get);
 router.patch("/:id", cookie.authorize, user_controller.user_update);
 router.delete("/:id", cookie.authorize, user_controller.user_delete);
 router.post("/unfollow/:id", cookie.authorize, user_controller.unfollow_post);

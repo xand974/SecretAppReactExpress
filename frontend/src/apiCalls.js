ddf79ca085fe363996156ebdce2ec07a1fef4568
Alexandre <xand974@gmail.com>
2021-07-30 11:53:43 +0200

    ajout reducer + apiCall pour login

---
+++ frontend/src/apiCalls.js
@@ -0,0 +1,9 @@
+import Api from "./config/axios";
+
+export const loginCall = async (userData, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+  try {
+    const res = await Api.post("/user/login", userData);
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+  } catch (err) {}
+};

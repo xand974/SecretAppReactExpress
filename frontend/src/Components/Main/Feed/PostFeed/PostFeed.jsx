f5c5eb3912acfb60d4c149573be808f244fe3d5f
Alexandre <xand974@gmail.com>
2021-08-01 13:24:01 +0200

    setup postfeed

--- frontend/src/Components/Main/Feed/PostFeed/PostFeed.jsx
+++ frontend/src/Components/Main/Feed/PostFeed/PostFeed.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import imageToImport from "../../../../Images/default-user-image.png";
 import { Album, EmojiEmotions, LocationCity } from "@material-ui/icons";
 import "./PostFeed.css";
+import AuthContext from "../../../../context/AuthContext";
+
 export default function PostFeed({ image }) {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="feed__post">
       <div className="feed__top">
